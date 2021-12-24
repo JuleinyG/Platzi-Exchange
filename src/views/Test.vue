453fe51a1a87c1d8ed7266a978a8d0a46967ad60
Juleiny Gonzalez <jhonnair.gonzalez19@gmail.com>
2021-12-23 20:11:25 -0500

    Introduccion a la Api

---
+++ src/views/Test.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="flex-col items-center text-center">
+    <h1 class="text-gray-700 text-6xl">Platzi Test</h1>
+    <p class="text-gray-600 text-xl text-center">
+      Proyecto para obtener las cotizaciones de las cryptomonedas mas
+      <br />Este proyecto es utilizado en el curo de Vue.js Basico de Platzi
+    </p>
+
+    <router-link to="/" class="mt-5 text-xl text-green-600 hover:underline"
+      >Volver a la pagina de Inicio</router-link
+    >
+  </div>
+</template>
